bf7a8b9a3b688ade03e94a4db1479457df9501b2
firasdakwar <58812435+firasdakwar@users.noreply.github.com>
2020-11-20 13:47:33 +0200

    Testing Game Initiation

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,16 +1,57 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import Model.Game;
 import Model.GameTimer;
-import Model.HighScoreEntity;
-import Model.SysData;
+import Model.Location;
+import Model.Piece;
+import Model.Player;
+import Model.Soldier;
+import Utils.PrimaryColor;
 
 
 public class Main {
 	public static void main(String[] args) {
 		
-
+		Game game = null;
 		
+		// scores & color should not be provided --- only name
+		Player[] players = {new Player("Jack", "White", 0),new Player("Jack", "Black", 0)};
+		
+		if(players != null) {
+			System.out.print("Player 1 : " + players[0].getNickname() + " || ");
+			System.out.println("Player 2 : " + players[0].getNickname());
+		}
 
+		ArrayList<Piece> pieces = new ArrayList<>();
+		
+		for(int i = 1 ; i <= 3 ; i++)
+			for(char c = 'A' ; c <= 'H' ; c+=2) {
+				if(i == 2 && c == 'A') c = 'B';
+				Piece soldier = new Soldier(PrimaryColor.WHITE, new Location(i,c));
+				pieces.add(soldier);
+			}
+		
+		for(int i = 6 ; i <= 8 ; i++)
+			for(char c = 'B' ; c <= 'H' ; c+=2) {
+				if(i == 7 && c == 'B') c = 'A';
+				Piece soldier = new Soldier(PrimaryColor.BLACK, new Location(i,c));
+				pieces.add(soldier);
+			}
+		
+		
+		try {
+			game.initiateGame(players, pieces);
+			game = Game.getInstance();
+						
+			game.getBoard().printBoard();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
 		
 //		try {
 //			GameTimer timer = new GameTimer();
