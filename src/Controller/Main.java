ab7fdfb2d7f13eaf10479bf90999c04651e099c1
firasdakwar <58812435+firasdakwar@users.noreply.github.com>
2020-11-21 17:28:36 +0200

    Implemented Game Controllers in Console

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,103 +1,74 @@
 package Controller;
 
-import java.util.ArrayList;
-import java.util.Random;
-
+import java.util.Scanner;
 import Model.Game;
-import Model.GameTimer;
-import Model.Location;
-import Model.Piece;
 import Model.Player;
-import Model.Soldier;
-import Utils.PrimaryColor;
 
 
 public class Main {
+	static Game game = null;
+	
 	public static void main(String[] args) {
 		
-		Game game = null;
-		
+		System.out.print("Enter player 1 nickname: ");
+		Scanner scanner = new Scanner(System.in);
+		String name1 = scanner.nextLine();
+		System.out.print("Enter player 2 nickname: ");
+		scanner = new Scanner(System.in);
+		String name2 = scanner.nextLine();
+
 		// scores & color should not be provided --- only name
-		Player[] players = {new Player("Jack", "White", 0),new Player("Jack", "Black", 0)};
+		Player[] players = {new Player(name1, "White", 0),new Player(name2, "Black", 0)};
 		
 		if(players != null) {
 			System.out.print("Player 1 : " + players[0].getNickname() + " || ");
-			System.out.println("Player 2 : " + players[0].getNickname());
+			System.out.println("Player 2 : " + players[1].getNickname());
 		}
-
-		ArrayList<Piece> pieces = new ArrayList<>();
-		
-		for(int i = 1 ; i <= 3 ; i++)
-			for(char c = 'A' ; c <= 'H' ; c+=2) {
-				if(i == 2 && c == 'A') c = 'B';
-				Piece soldier = new Soldier(PrimaryColor.WHITE, new Location(i,c));
-				pieces.add(soldier);
-			}
-		
-		for(int i = 6 ; i <= 8 ; i++)
-			for(char c = 'B' ; c <= 'H' ; c+=2) {
-				if(i == 7 && c == 'B') c = 'A';
-				Piece soldier = new Soldier(PrimaryColor.BLACK, new Location(i,c));
-				pieces.add(soldier);
-			}
 		
 		
 		try {
-			game.initiateGame(players, pieces);
-			game = Game.getInstance();
-						
-			game.getBoard().printBoard();
+			game = new Game(players);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 			System.exit(1);
 		}
+		System.out.println("Write /start to start the game");
+		System.out.println("Please enter a command:");
 		
-//		try {
-//			GameTimer timer = new GameTimer();
-//			timer.startTimer(500);
-//			System.out.println(timer.getSeconds() + " ~~ 500");
-
-//			Thread.sleep(5000);
-
-//			System.out.println(timer.getSeconds() + " ~~ 505");
-//
-//		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-//		try {	
-//			GameTimer timer = new GameTimer();
-//			timer.startTimer();
-//		
-//			Thread.sleep(5000);
-//			
-//			System.out.println(timer.getSeconds() + " ~~ 5");
-//			
-//			timer.pauseTimer();
-//			
-//			Thread.sleep(5000);
-//			
-//			System.out.println(timer.getSeconds() + " ~~ 5");
-//			
-//			timer.unpauseTimer();
-//			
-//			Thread.sleep(5000);
-//			
-//			System.out.println(timer.getSeconds() + " ~~ 10");
-//			
-//			timer.resetTimer();
-//			System.out.println(timer.getSeconds() + " ~~ 0");
-//			
-//			Thread.sleep(5000);
-//			
-//			System.out.println(timer.getSeconds() + " ~~ 5");
-//			
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
+		while(true) {
+			scanner = new Scanner(System.in);
+			String str = scanner.nextLine();
+			if(str.charAt(0) == '/') {
+				command(str.substring(1));
+			}
+		}
+				
+	}
+	
+	
+	private static void command(String cmd) {
+		switch(cmd) {
+			case "start":
+				game.startGame();
+//				game.standardGameTiles();
+//				game.getBoard().printBoard();
+				break;
+			case "pause":
+				game.pauseGame();
+				break;
+			case "unpause":
+				game.unpauseGame();
+				break;
+			case "board":
+				game.getBoard().printBoard();
+				break;
+			case "exit":
+				System.out.println("Game Terminated.");
+				System.exit(1);
+				break;
+			default:
+				System.err.println("Invalid Command");
+		}
 	}
 }
