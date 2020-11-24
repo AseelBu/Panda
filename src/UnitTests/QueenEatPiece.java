4d56fe2a98d087ba6161d42ab3ae9d4e9ca61cd6
firasdakwar <58812435+firasdakwar@users.noreply.github.com>
2020-11-25 00:05:30 +0200

    Unit Tests on Queen moves

---
+++ src/UnitTests/QueenEatPiece.java
@@ -0,0 +1,50 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import Model.Game;
+import Model.Location;
+import Model.Piece;
+import Model.Player;
+import Model.Queen;
+import Model.Soldier;
+import Utils.Directions;
+import Utils.PrimaryColor;
+
+public class QueenEatPiece {
+
+	static final int ROW_LOCATION = 7;
+	static final char COLUMN_LOCATION = 'C';
+	static final Directions DIR = Directions.UP_LEFT;
+
+	
+	@Test
+	public void test() {
+		Player[] players = new Player[] {new Player(),new Player()};
+		ArrayList<Piece> pieces = new ArrayList<>();
+		Piece queen = new Queen(1, PrimaryColor.WHITE, new Location(1,'A'));
+		Piece soldier = new Soldier(2, PrimaryColor.BLACK, new Location(6,'F'));
+		Piece soldier2 = new Soldier(3, PrimaryColor.BLACK, new Location(5,'E'));
+		
+		pieces.add(queen);
+		pieces.add(soldier);
+		pieces.add(soldier2);
+
+		try {
+			Game.getInstance().startGame(players, pieces);
+			Game.getInstance().getBoard().printBoard();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Queen queen2 = (Queen) Game.getInstance().getBoard().getTilesMap().get(queen.getLocation().getRow()).get(queen.getLocation().getColumn() - 'A').getPiece();
+		Piece p = queen2.getEdiblePieceByDirections(Game.getInstance().getBoard().getTilesMap().get(ROW_LOCATION).get(COLUMN_LOCATION - 'A').getLocation(),DIR);
+		assertEquals("F6 To be Eaten", "F6",p.getLocation().getColumn() + "" + p.getLocation().getRow());
+
+	}
+
+}
