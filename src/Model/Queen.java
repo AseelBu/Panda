74943495c0e43f36753b5a3bea63ca35ba13af30
firasdakwar <58812435+firasdakwar@users.noreply.github.com>
2020-11-24 19:29:53 +0200

    Implemented Queen methods

--- src/Model/Queen.java
+++ src/Model/Queen.java
@@ -1,7 +1,9 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import Utils.Directions;
 import Utils.PrimaryColor;
 
 public class Queen extends Piece{
@@ -20,24 +22,504 @@ public class Queen extends Piece{
 
 	@Override
 	public void move(Tile targetTile) {
-		// TODO Auto-generated method stub
-		
+//		Location targetLocation = targetTile.getLocation();
+//		if(this.isMoveLegal(targetLocation)) {
+//			if(Board.getInstance().canPieceMove(this, targetLocation)) {
+//				//			TODO does moving contains eating?
+//				this.setLocation(targetLocation);
+//			}
+//		}
+	}
+	
+	/**
+	 * Method used to move a piece by selected direction
+	 * @param targetTile
+	 * @param direction
+	 */
+	public void move(Tile targetTile, Directions direction) {
+		Location targetLocation = targetTile.getLocation();
+		if(this.isMoveLegal(targetLocation)) {
+			if(Board.getInstance().canPieceMove(this, targetLocation, direction)) {
+				//			TODO does moving contains eating?
+				this.setLocation(targetLocation);
+			}
+		}
 	}
 
 	@Override
-	public boolean isMoveLegal(Location targetLocation) {
+	public boolean isMoveLegal(Location targetLocation) { // NOT DONE
+		Location currentLocation = getLocation();
+		if(currentLocation.getRow() == targetLocation.getRow() ||
+				currentLocation.getColumn() == targetLocation.getColumn())
+			return false;
+		if(currentLocation.getRow() - targetLocation.getRow() != currentLocation.getColumn() - targetLocation.getColumn()) {
+			isMoveLegalByDirection(targetLocation, Directions.UP_LEFT);
+			isMoveLegalByDirection(targetLocation, Directions.UP_RIGHT);
+			isMoveLegalByDirection(targetLocation, Directions.DOWN_LEFT);
+			isMoveLegalByDirection(targetLocation, Directions.DOWN_RIGHT);
+		}
 		
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 		
 	}
+	/**
+	 * Checks whether the move is legal by direction
+	 * @param targetLocation
+	 * @param direction
+	 * @return true if it is legal, otherwise false
+	 */
+	public boolean isMoveLegalByDirection(Location targetLocation, Directions direction) {
+		if(getLocation().getRow() == targetLocation.getRow() || getLocation().getColumn() == targetLocation.getColumn()) return false;
+		switch(direction) {
+			case UP_LEFT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
 
+				do {
+					if(targetLocation.getRow() == i && targetLocation.getColumn() == c) return true;
+					i++;
+					c++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+
+				break;
+			}
+			case UP_RIGHT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+
+				do {
+					if(targetLocation.getRow() == i && targetLocation.getColumn() == c) return true;
+					i++;
+					c++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+				
+				break;
+			}
+			case DOWN_LEFT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+
+				do {
+					if(targetLocation.getRow() == i && targetLocation.getColumn() == c) return true;
+					i++;
+					c++;
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+				
+				break;
+			}
+			case DOWN_RIGHT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+
+				do {
+					if(targetLocation.getRow() == i && targetLocation.getColumn() == c) return true;
+					i++;
+					c++;
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+				
+				break;
+			}
+			default: return false;
+		}
+		
+		return true;
+	}
 	
 	
 	@Override
 	public ArrayList<Piece> getEdiblePieces() {
-		// TODO Auto-generated method stub
+		ArrayList<Piece> pieces = new ArrayList<>();
+		Piece temp = null;
+		temp  = getEdiblePieceByDirections(Directions.UP_LEFT);
+		if(temp != null)
+			pieces.add(temp);
+		temp  = getEdiblePieceByDirections(Directions.UP_RIGHT);
+		if(temp != null)
+			pieces.add(temp);
+		temp  = getEdiblePieceByDirections(Directions.DOWN_LEFT);
+		if(temp != null)
+			pieces.add(temp);
+		temp  = getEdiblePieceByDirections(Directions.DOWN_RIGHT);
+		if(temp != null)
+			pieces.add(temp);
+		
+		return pieces;
+	}
+	
+	/**
+	 * 
+	 * @param direction
+	 * @return Object of Edible Piece, otherwise null
+	 */
+	public Piece getEdiblePieceByDirections(Directions direction){
+		Board board = Board.getInstance();
+		HashMap<Integer, ArrayList<Tile>> boardMap = board.getTilesMap();
+		Piece suspectedToAdd = null;
+		
+		switch(direction) {
+			case UP_LEFT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToAdd != null) break;
+						else {
+							suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToAdd != null) {
+							return suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}
+					i++;
+					c++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+	
+				break;
+			}
+			case UP_RIGHT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToAdd != null) break;
+						else {
+							suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToAdd != null) {
+							return suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+
+						}
+					}
+					i++;
+					c++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+
+				break;
+			}
+			case DOWN_LEFT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToAdd != null) break;
+						else {
+							suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToAdd != null) {
+							return suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}
+					i++;
+					c++;
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+
+				break;
+			}
+			case DOWN_RIGHT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToAdd != null) break;
+						else {
+							suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToAdd != null) {
+							return suspectedToAdd = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}
+					i++;
+					c++;
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while(i != getLocation().getRow() && c != getLocation().getColumn());
+
+				break;
+			}
+			default: return null;
+		}
+		
 		return null;
+	}
+	
+	public Piece getEdiblePieceByDirections(Location targetLocation, Directions direction) {
+		Board board = Board.getInstance();
+		HashMap<Integer, ArrayList<Tile>> boardMap = board.getTilesMap();
+		Piece suspectedToBlock = null;
+		
+		switch(direction) {
+			case UP_LEFT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return null;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							return suspectedToBlock;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+	
+				break;
+			}
+			case UP_RIGHT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return null;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							return suspectedToBlock;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+	
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+	
+				break;
+			}
+			case DOWN_LEFT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return null;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							return suspectedToBlock;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+	
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+	
+				break;
+			}
+			case DOWN_RIGHT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+	
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return null;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							return suspectedToBlock;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+	
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+	
+				break;
+			}
+			default: return null;
+		}
+		return null;
+	}
+	
+	public boolean isPieceBlockedByDirection(Location targetLocation, Directions direction) {
+		Board board = Board.getInstance();
+		HashMap<Integer, ArrayList<Tile>> boardMap = board.getTilesMap();
+		Piece suspectedToBlock = null;
+		
+		switch(direction) {
+			case UP_LEFT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return true;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							suspectedToBlock = null;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+	
+				break;
+			}
+			case UP_RIGHT:{
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return true;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							suspectedToBlock = null;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+
+					if(i > Board.getInstance().getBoardSize()) i = 1;
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+
+				break;
+			}
+			case DOWN_LEFT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return true;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							suspectedToBlock = null;						}
+					}
+					i++;
+					c++;
+					counter++;
+
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnLowerbond()) c = Board.getInstance().getColumnUpperbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+
+				break;
+			}
+			case DOWN_RIGHT:{//
+				
+				int i = getLocation().getRow();
+				int c = getLocation().getColumn();
+				int counter = 1;
+
+				do {
+					if(boardMap.get(i).get('H' - c).getPiece() != null) {
+						if(suspectedToBlock != null) return true;
+						else {
+							suspectedToBlock = boardMap.get(i).get('H' - c).getPiece();
+						}
+					}else {
+						if(suspectedToBlock != null) {
+							suspectedToBlock = null;
+						}
+					}
+					i++;
+					c++;
+					counter++;
+
+					if(i == 1) i = Board.getInstance().getBoardSize();
+					if(c > Board.getInstance().getColumnUpperbond()) c = Board.getInstance().getColumnLowerbond();
+				}
+				while((i != targetLocation.getRow() && c != targetLocation.getColumn() ) &&  counter <= 8);
+
+				break;
+			}
+			default: return false;
+		}
+		
+		return false;
 	}
 	
 	@Override
