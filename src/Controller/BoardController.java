a9eaaf275d0055a7ff14faeca71619d75c23aff6
AseelBu <aseel_bu@hotmail.com>
2020-12-06 16:48:33 +0200

    moved moviePiece() logic from Board to BoardController

--- src/Controller/BoardController.java
+++ src/Controller/BoardController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import Exceptions.IllegalMoveException;
 import Exceptions.LocationException;
@@ -8,8 +9,11 @@ import Model.Board;
 import Model.Game;
 import Model.Location;
 import Model.Piece;
+import Model.Player;
+import Model.Queen;
 import Model.Soldier;
 import Model.Tile;
+import Model.Turn;
 import Utils.Directions;
 import Utils.PrimaryColor;
 import View.BoardGUI;
@@ -74,9 +78,74 @@ public class BoardController {
 	 * @throws IllegalMoveException 
 	 */
 	public boolean movePiece(int fromRow, char fromCol, int toRow, char toCol, Directions direction) throws IllegalMoveException, LocationException {
+		Board board =Board.getInstance();
 		Location fromLocation = new Location(fromRow, fromCol);
 		Location toLocation = new Location(toRow, toCol);
-		return Board.getInstance().movePiece(fromLocation, toLocation, direction);
+		System.out.println("Attempting to move piece from: " + fromCol + "" + fromRow + " | to : " + toCol + "" + toRow);
+		HashMap<Piece, ArrayList<Piece>> toBurn = board.searchToBurn();
+		Player currPlayer =Game.getInstance().getTurn().getCurrentPlayer();
+		Piece piece = null;
+		Tile fromTile = null, toTile = null;
+		try {
+			fromTile = board.getTileInLocation(fromLocation);
+			toTile = board.getTileInLocation(toLocation);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+		if(fromTile == null || toTile == null) {
+			System.out.println("Invalid Locations..");
+			return false;
+		}
+		piece = fromTile.getPiece();
+
+		if(piece == null) {
+			board.printBoard();
+			System.out.println("Tile has no piece!");
+			return false;
+		}
+		if(currPlayer.getColor() != piece.getColor()) {
+			throw new IllegalMoveException("You cannot move your opponent's piece");
+		}
+
+		if(piece.move(toTile, direction)) {
+			Turn turn = Game.getInstance().getTurn();
+			if(toBurn.containsKey(piece)) {
+				ArrayList<Piece> temp = toBurn.get(piece);
+				toBurn.remove(piece);
+				toBurn.put(turn.getLastPieceMoved(), temp);
+			}
+			board.burnAllPiecesMissedEating(toBurn);
+
+			if(turn.getMoveCounter()>0) {
+				Game.getInstance().getTurn().decrementMoveCounter();
+			}
+			
+			// TODO step on handling not finished
+			toTile.stepOnBy(piece);
+			
+			if(piece.getLocation() != null)
+				if(piece instanceof Queen) System.out.println("Queen has been moved!");
+				else System.out.println("Soldier has been moved!");
+
+			// is there eating left for the piece
+			if(!board.isAllPiecesEaten(Game.getInstance().getTurn().getLastPieceMoved()) && 
+					Game.getInstance().getTurn().getLastPieceMoved().getEatingCntr() > 0) {
+				Game.getInstance().getTurn().IncrementMoveCounter();
+			}
+			
+			if(turn.getMoveCounter() == 0) {
+				Game.getInstance().switchTurn(); // TODO Add conditions on move counter - move piece more than once
+			}
+
+			if(board.getColorPieces(PrimaryColor.WHITE).size() == 0 || board.getColorPieces(PrimaryColor.BLACK).size() == 0)
+				Game.getInstance().finishGame();
+			return true;
+		}
+
+
+		return false;
+		//return Board.getInstance().movePiece(fromLocation, toLocation, direction);
 	}
 	
 	/**
