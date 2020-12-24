b6c23912768407b6a6d89fbc7aebcccfba25cf86
סאלח גרייב <sghraiyi@campus.haifa.ac.il>
2020-12-24 18:22:21 +0200

    Muted Until Buttons Are Added

--- src/Controller/SoundController.java
+++ src/Controller/SoundController.java
@@ -34,25 +34,25 @@ public class SoundController {
 		public SoundController() {
 			
 			
-			songs.put("burn", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\burn.mp3").toURI().toString()));
-			songs.put("intro", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\bg_music.mp3").toURI().toString()));
-			songs.put("ques", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\30.mp3").toURI().toString()));
-			songs.put("move", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\move.wav").toURI().toString()));
+			//songs.put("burn", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\burn.mp3").toURI().toString()));
+			//songs.put("intro", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\bg_music.mp3").toURI().toString()));
+			//songs.put("ques", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\30.mp3").toURI().toString()));
+			//songs.put("move", new Media(new File("C:\\Users\\97254\\Desktop\\SoundFX\\move.wav").toURI().toString()));
 			
-			this.introPlayer = new MediaPlayer(songs.get("intro"));
-			this.burnPlayer = new MediaPlayer(songs.get("burn"));
-			this.quesPlayer = new MediaPlayer(songs.get("ques"));
-			this.movePlayer = new MediaPlayer(songs.get("move"));
+			//this.introPlayer = new MediaPlayer(songs.get("intro"));
+			//this.burnPlayer = new MediaPlayer(songs.get("burn"));
+			//this.quesPlayer = new MediaPlayer(songs.get("ques"));
+			//this.movePlayer = new MediaPlayer(songs.get("move"));
 			
-			introPlayer.setVolume(0.0);
-			burnPlayer.setVolume(0.0);
-			quesPlayer.setVolume(0.0);
+			//introPlayer.setVolume(0.0);
+			//burnPlayer.setVolume(0.0);
+			//quesPlayer.setVolume(0.0);
 			
-			introPlayer.setOnEndOfMedia(new Runnable() {
-			       public void run() {
-			    	   introPlayer.seek(Duration.ZERO);
-			       }
-			   });
+			//introPlayer.setOnEndOfMedia(new Runnable() {
+			       //public void run() {
+			    	  // introPlayer.seek(Duration.ZERO);
+			      // }
+			 //  });
 			
 			
 			
@@ -60,34 +60,34 @@ public class SoundController {
 		}
 		
 		public void playIntro() {
-			this.introPlayer.play();
+			//this.introPlayer.play();
 		}
 		
 		public void stopIntro() {
-			this.introPlayer.stop();
+			// this.introPlayer.stop();
 		}
 		
 		public void play30() {
 			
-			this.quesPlayer.play();
+			// this.quesPlayer.play();
 			
 		}
 		
 		public void stopQues() {
 			
-			this.quesPlayer.stop();
+			// this.quesPlayer.stop();
 		}
 		
 		public void playBurn() {
 			
-			this.burnPlayer.play();
-			this.burnPlayer.seek(Duration.ZERO);
+		//	this.burnPlayer.play();
+		//	this.burnPlayer.seek(Duration.ZERO);
 		}
 		
 		public void playMove() {
 			
-			this.movePlayer.play();
-			this.movePlayer.seek(Duration.ZERO);
+			//this.movePlayer.play();
+			//this.movePlayer.seek(Duration.ZERO);
 			
 		}
 
@@ -96,17 +96,17 @@ public class SoundController {
 		
 		public void muteSound() {
 			
-			this.burnPlayer.setVolume(0);
-			this.introPlayer.setVolume(0);
-			this.quesPlayer.setVolume(0);
+			//this.burnPlayer.setVolume(0);
+			//this.introPlayer.setVolume(0);
+			//this.quesPlayer.setVolume(0);
 			
 		}
 		
 		public void unmuteSound() {
 			
-			this.burnPlayer.setVolume(0.7);
-			this.introPlayer.setVolume(0.7);
-			this.quesPlayer.setVolume(0.7);
+			//this.burnPlayer.setVolume(0.7);
+			//this.introPlayer.setVolume(0.7);
+		   //this.quesPlayer.setVolume(0.7);
 			
 		}
 			
